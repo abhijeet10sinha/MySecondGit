4cf4cf5b55db2a78ebe71ccce170992105029984
abhijeet10sinha <abhijeetsinha0410@gmail.com>
2023-03-30 08:41:14 +0530

    Now the data stored as an Object in local storage.

--- main.js
+++ main.js
@@ -1,68 +1,49 @@
-// const ul = document.querySelector('.items');
-
-// let c = ul.firstElementChild.textContent = 'HELLO';
-// console.log(ul.firstElementChild.style.color = 'green');
-// console.log(ul.children[1].style.color = 'yellow');
-
-// const btn = document.querySelector('.btn');
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     console.log('click');
-// })
-
-// btn.addEventListener('mouseover', (e) => {
-//     e.preventDefault();
-//     document.querySelector('#my-form').style.background = 'cyan';
-// })
-
-// btn.addEventListener('mouseout', (e) => {
-//     e.preventDefault();
-//     document.querySelector('#my-form').style.background = 'pink';
-// })
-
-
-//grabbing the bunch of doms and put them into variables.
-
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
+// Check for existing data in local storage
+let userData = localStorage.getItem('userData');
+if (userData === null) {
+  // If no existing data, initialize userData as an empty array
+  userData = [];
+} else {
+  // If existing data, parse the JSON string into a JavaScript object
+  userData = JSON.parse(userData);
+}
+
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (nameInput.value === '' || emailInput.value === '') {
-        msg.classList.add('error');
-        msg.innerHTML = 'please enter the correct inputs';
+  if (nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter the correct inputs';
 
-        setTimeout(() => msg.remove(), 3000);
-    }
-    else {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    // Create a new user object with name and email properties
+    const newUser = {
+      name: nameInput.value,
+      email: emailInput.value
+    };
 
-        userList.appendChild(li);
+    // Add the new user object to the userData array
+    userData.push(newUser);
 
-        nameInput.value = '';
-        emailInput.value = '';
-    }
-}
+    // Store the updated userData array in local storage as a JSON string
+    localStorage.setItem('userData', JSON.stringify(userData));
 
-window.onload = function(){
-    //checking for local storage support.
-    if(localStorage){
-        //Add an event listner for form submission
-        document.getElementById('my-form').addEventListener('submit', function(){
-            //get the value of the name field
-            let name = document.getElementById('name').value;
-            let email = document.getElementById('email').value;
+    // Add the new user to the user list
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${newUser.name} : ${newUser.email}`));
+    userList.appendChild(li);
 
-            //Save the name and email in local storage
-            localStorage.setItem('name', name);
-            localStorage.setItem('email', email);
-        })
-    }
+    // Clear the form inputs
+    nameInput.value = '';
+    emailInput.value = '';
+  }
 }
